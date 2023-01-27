75e48f1420c273e76e7ca17b1f8193b81e511322
LEONARDO OLIVEIRA SOUZA <88562663+Leonardu76@users.noreply.github.com>
2023-01-27 20:53:49 -0300

    alte

--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './resume.css'
 import Menu from '../../components/menu/menu'
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 
 
 const Resume = () => {
@@ -19,7 +20,41 @@ const Resume = () => {
 
                 <h1>Resume</h1>
 
-            </div>        </div>
+                <Timeline >
+                    <TimelineItem
+                        key="001"
+                        dateText="12/2022 – Present"
+                    >
+                        <h3 className='resume-title'>Desenvolvedor Web Jr</h3>
+                        <h4 className='resume-title'>Construsite Brasil</h4>
+                        <p className='resume-description'>
+                        Programação, alteração e correção de sites em PHP.
+                        </p>
+                    
+                   
+                    </TimelineItem>
+                    <TimelineItem
+                        key="002"
+                        dateText="08/2022 – 12/2022">
+                        <h3  className='resume-title'>Estágiario de Desenvolvimento Web</h3>
+                        <h4 className='resume-title'>Construsite Brasil</h4>
+                        <p className='resume-description'>
+                        Programação, alteração e correção de sites em PHP.
+                        </p>
+                    </TimelineItem>
+                    <TimelineItem
+                        key="003"
+                        dateText="08/2022 – 12/2022">
+                        <h3 className='resume-title'>Whobots</h3>
+                        <h4 className='resume-title'>Desenvolvedor Python</h4>
+                        <p className='resume-description'>
+                            Auxilio no desenvolvimento de chatbots para o Telegram.
+                        </p>
+                    </TimelineItem>
+
+                </Timeline>
+            </div>
+        </div>
 
     )
 }
