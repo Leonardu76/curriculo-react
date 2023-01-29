69d57feed0e91dcb82842e6f486bfcc471f3bafc
LEONARDO OLIVEIRA SOUZA <88562663+Leonardu76@users.noreply.github.com>
2023-01-28 21:57:58 -0300

    a

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,25 +1,59 @@
-import React from 'react'
 import './contact.css'
 import Menu from '../../components/menu/menu'
+import React, { useState } from "react";
+import { BiMailSend } from "react-icons/bi";
+
 
 
 const Contact = () => {
-    // const [data, setData] = useState([])
+    const [nome, setNome] = useState()
+    const [email, setEmail] = useState()
+    const [assunto, setAssunto] = useState()
+
 
     let url = 'contact'
 
 
 
     return (
-        <div className='col-md-12 row'>
-            <div className='col-md-5'>
+        <div>
+            <div>
                 <Menu url={url} />
             </div>
-            <div className='home col-md-5'>
+            <div className='container '>
+                <div className='contact-content'>
+                    <h1>Contact</h1>
+                    <div className='form-contact-background '></div>
+                    <div className='form-contact'>
+                    <div className='label-float'>
+                        <input className='inputForm col-md-6' type="text" name='name'
+                            placeholder=" " />
+                        <label><b>Nome</b></label>
+                    </div>
 
-                <h1>Contact</h1>
+                    <div className='label-float'>
+                        <input className='inputForm col-md-6' type="text" name='name'
+                            placeholder=" " />
+                        <label><b>Email</b></label>
+                        </div>
 
-            </div>        </div>
+
+                        <div className='label-float'>
+                        <textarea className='inputForm col-md-10' placeholder=" " rows="7"></textarea>
+                        <label><b>Assunto</b></label>
+                        </div>
+                        <button className=' button-inter item9 btn-send-email' >
+                <BiMailSend/>
+                Enviar
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+                        </div>
+                </div>
+            </div>
+            </div>
 
     )
 }
