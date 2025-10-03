cc021c92675aa956df5d6e7b87e3aebf1a586c7f
LEONARDO OLIVEIRA SOUZA <leonardudesouza@gmail.com>
2025-10-02 21:51:39 -0300

    feat: ajustes

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./menu.css";
 import { Link } from "react-router-dom";
 import { SlSocialLinkedin } from "react-icons/sl";
@@ -18,79 +18,79 @@ function ThemeSwitcher() {
     </div>
   );
 }
+
 const Menu = (props) => {
   const { url } = props;
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <ThemeProvider>
-      <div className="menu-card col-12 col-md-3 col-lg-2">
+      <button
+        className={`hamburger ${isOpen ? "active" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+
+      <div className={`menu-card col-12 col-md-3 col-lg-2 ${isOpen ? "open" : ""}`}>
         <div className="menu-ul">
           <Link to={"/"} className={url === "home" ? "active " : "menu-li"}>
             <span>Início</span>
           </Link>
-          <div>
-            <Link
-              to={"/about"}
-              className={url === "about" ? "active" : "menu-li"}
-            >
-              <span>Sobre</span>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to={"/resume"}
-              className={url === "resume" ? "active" : "menu-li"}
-            >
-              <span>Experiências</span>
-            </Link>
-          </div>
-          <div>
-            <Link
-              to={"/portfolio"}
-              className={url === "portfolio" ? "active" : "menu-li"}
-            >
-              <span>Projetos</span>
-            </Link>
-          </div>
-
-          <div>
-            <Link
-              to={"/contact"}
-              className={url === "contact" ? "active" : "menu-li"}
-            >
-              <span>Contato</span>
-            </Link>
-          </div>
+          <Link to={"/about"} className={url === "about" ? "active" : "menu-li"}>
+            <span>Sobre</span>
+          </Link>
+          <Link
+            to={"/resume"}
+            className={url === "resume" ? "active" : "menu-li"}
+          >
+            <span>Experiências</span>
+          </Link>
+          <Link
+            to={"/portfolio"}
+            className={url === "portfolio" ? "active" : "menu-li"}
+          >
+            <span>Projetos</span>
+          </Link>
+          <Link
+            to={"/contact"}
+            className={url === "contact" ? "active" : "menu-li"}
+          >
+            <span>Contato</span>
+          </Link>
         </div>
 
-        <div class="redes-menu">
+        <div className="redes-menu">
           <a
             target={"_blank"}
             rel="noreferrer"
             href="https://www.linkedin.com/in/leonardo-oliveira-3b0446176/"
-            class="icon-border-menu"
+            className="icon-border-menu"
           >
-            <SlSocialLinkedin class="icon" />
+            <SlSocialLinkedin className="icon" />
           </a>
 
           <a
             target={"_blank"}
             rel="noreferrer"
             href="https://github.com/Leonardu76"
-            class="icon-border-menu"
+            className="icon-border-menu"
           >
-            <VscGithubAlt class="icon" />
+            <VscGithubAlt className="icon" />
           </a>
 
           <a
             target={"_blank"}
             rel="noreferrer"
             href="https://api.whatsapp.com/send?phone=5531985808502&text=Olá! Vim através do site"
-            class="icon-border-menu"
+            className="icon-border-menu"
           >
-            <AiOutlineWhatsApp class="icon" />
+            <AiOutlineWhatsApp className="icon" />
           </a>
         </div>
+
         <ThemeSwitcher />
       </div>
     </ThemeProvider>
